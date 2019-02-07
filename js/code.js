dc0d29884a6477826b1e97c2699231ca6de9edfa
anngelRuiz <anngelruiz@hotmail.com>
2019-02-07 16:12:16 -0700

    navbar link with smooth scrolling

--- js/code.js
+++ js/code.js
@@ -1,12 +1,55 @@
 $(document).ready(function () {
-    // Transition effect for navbar 
-    $(window).scroll(function () {
-        // checks if window is scrolled more than 500px, adds/removes solid class
-        if ($(this).scrollTop() > 80) {
-            $('.navbar').addClass('solid');
-        } else {
-            $('.navbar').removeClass('solid');
-        }
+
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 80) {
+    //         $('.navbar').addClass('solid');
+    //     } else {
+    //         $('.navbar').removeClass('solid');
+    //     }
+
+    //     if ($(this).scrollTop() >= $('#section-work').position().top) {
+    //         yourActionHere();
+    //     }
+    // });
+
+
+
+    $(document).on("scroll", onScroll);
+
+    function onScroll(event) {
+        var scrollPos = $(document).scrollTop();
+
+        scrollPos > 80 ? $('.navbar').addClass('solid') : $('.navbar').removeClass('solid');
+
+        $('#nav ul li a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('#nav ul li a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else {
+                currLink.removeClass("active");
+            }
+        });
+    }
+
+    // section - home
+    // section - work
+    // about - me
+    // section - contact
+
+    $("#nav ul li a[href^='#']").on('click', function (e) {
+        e.preventDefault();
+        var hash = this.hash;
+        var scrollValue = hash != "#section-home" ? scrollValue = $(hash).offset().top : 0;
+
+        $('html, body').stop().animate({ scrollTop: scrollValue }, 700, function () {
+            // window.location.hash = hash;
+        });
+
+        $('#nav ul li a').not(this).removeClass('active');
+        $(this).addClass('active');
     });
 });
 
@@ -15,7 +58,7 @@ console.log("/.&&                                                               
 console.log("/..&&&                                                               &&#,/&");
 console.log("(#/.&&&&                                                           &&&#,/&");
 console.log("(#&*,,,,,&#                                                       &&&&#,/&");
-console.log("((#&*,,,,,,,&#                                                  &&&&&&#,/&"); //n
+console.log(" (#&*,,,,,,,&#                                                  &&&&&&#,/&"); //n
 console.log("  ((#&*,,,,,,,,###                                             &,,,,,,,&&#");
 console.log("   ((&*,,,,,,,,,,##                                          &,,,,,,,,,,&&");
 console.log("       (*,,,,,,,,,,,,,##   #&&&&&&&&&&&&&&&&&&&#        &,,,,,,,,,,,,,,,&#");
